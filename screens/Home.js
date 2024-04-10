f75478c542479ad3415cc96ae090720e83d53656
ValerieLove <valerievictoriavalentine@gmail.com>
2024-04-10 16:49:20 -0500

    added swipe cards

--- screens/Home.js
+++ screens/Home.js
@@ -1,51 +1,60 @@
 import React from "react";
-import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { StyleSheet, Dimensions, View } from "react-native";
 import { Block, theme, Text } from "galio-framework";
-
+import Swiper from 'react-native-deck-swiper'
 import { Card } from "../components";
 import articles from "../constants/articles";
 const { width } = Dimensions.get("screen");
 
 class Home extends React.Component {
-  renderArticles = () => {
-    return (
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.articles}
-      >
-        <Block flex>
-        <Card item={articles[0]} horizontal /> 
-          <Block flex row>
-            <Card
-              item={articles[1]}
-              style={{ marginRight: theme.SIZES.BASE }}
-            />
-            <Card item={articles[2]} />
-          </Block>
-          <Card item={articles[3]} horizontal />
-          <Card item={articles[4]} full />
-        </Block>
-      </ScrollView>
-    );
+  renderJobs = () => {
+    return(
+    <View style={styles.container}>
+      <Swiper
+        cards={[{text: "Hoyack"},{text: "is"},{text: "based"},{text:"and"},{text:"redpilled"}]}
+        renderCard={(card) => (
+          <View style={styles.card}>
+            <Text style={styles.text}>
+              {card.text}
+            </Text>
+
+          </View>
+        )}
+        onSwiped={(cardIndex) => { console.log(cardIndex) }}
+        onSwipedAll={() => { console.log('onSwipedAll') }}
+        cardIndex={0}
+        backgroundColor={'#C0FFEE'}
+        stackSize={5}>
+      </Swiper>
+    </View>
+    )
   };
 
   render() {
     return (
-      <Block flex center style={styles.home}>
-        {this.renderArticles()}
-      </Block>
+        this.renderJobs()
     );
   }
 }
 
 const styles = StyleSheet.create({
-  home: {
-    width: width
+  container: {
+    flex: 1,
+    backgroundColor: "#C0FFEE",
   },
-  articles: {
+  card: {
+    flex: 1,
     width: width - theme.SIZES.BASE * 2,
-    paddingVertical: theme.SIZES.BASE,
-    paddingHorizontal: 2
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
   }
 });
 
